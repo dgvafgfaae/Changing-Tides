bd58e37d9bd3f6a5c46b55c307ab224524e61a87
dgvafgfaae <136701042+dgvafgfaae@users.noreply.github.com>
2023-07-17 06:41:47 +0700

    Fix broken error handling

---
+++ tree_flower_house.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// The Changing Tides program is designed to help people understand the effects of changing tides in the ocean
+
+func main() {
+	fmt.Println("Welcome to Changing Tides!")
+
+	// This section of the program will explain the principles of tides
+	fmt.Println("Tides are created by the gravitational pull of the Sun and Moon on the ocean. They rise and fall twice in twenty-four hours, creating high and low tides.")
+ 
+	// This part will discuss the different types of tides 
+	fmt.Println("There are two main types of tides: diurnal and semidiurnal. Diurnal tides occur only once a day, while semidiurnal tides occur twice a day.")
+ 
+	// This part of the program will explain how tides are affected by the moon
+	fmt.Println("The moon's gravitational pull is the strongest force that affects the tides. The closer the moon is to the Earth, the higher the tidal range.")
+ 
+	// This section will explore how the sun affects tides
+	fmt.Println("The Sun also influences the tides, but its gravitational pull is much weaker than the moon's. When the Sun and Moon are in the same or opposite positions, their combined gravitational pull creates extra-high tides called spring tides.")
+ 
+	// This part of the program will explain how changing tides can affect marine life
+	fmt.Println("Changing tides can have a big impact on marine life. For example, some animals that live in tidal zones feed on plankton and seaweed that are swept up by incoming tides. These animals must time their activities to take advantage of the tides.")
+ 
+	// This section will discuss how humans use changing tides
+	fmt.Println("Humans have also been using the changing tides for centuries. Fishermen use them to navigate to areas with better fishing opportunities. And in many coastal areas, people use the tides to predict the weather.")
+ 
+	// This part of the program will explain the dangers of changing tides
+	fmt.Println("There are also dangers associated with changing tides. In some areas, the tidal range can become dangerously high, posing a risk to boats and other vessels.")
+ 
+	fmt.Println("Thank you for using Changing Tides. We hope you have a better understanding of tides and how they affect us.")
+}
